7319b7171541a84ccfdeb2e8d52e36259470d508
salmaBadr27 <salmabadr27795@gmail.com>
2019-10-24 00:03:21 +0200

    routing and some directives

--- app/app.js
+++ app/app.js
@@ -1,18 +1,87 @@
-var myApp = angular.module("myApp", []);
+//module with its dependencies
+var myApp = angular.module("myApp", ["ngRoute"]);
 
-// //function run before your app run
-// myApp.config(function() {});
+// //function run before your app start
+myApp.config([
+  "$routeProvider",
+  function($routeProvider) {
+    $routeProvider
+      .when("/home", {
+        templateUrl: "views/home.html"
+      })
+      .when("/directory", {
+        templateUrl: "views/directory.html",
+        controller: "myController"
+      })
+      .otherwise({
+        redirectTo: "/home"
+      });
+  }
+]);
 
-// //function run when your app run
-// myApp.run(function() {});
-
+//controller which wraps all functionality of our view or main view
 myApp.controller("myController", [
   "$scope",
   function($scope) {
-    $scope.names = [
-      { name: "first", rate: 50 },
-      { name: "second", rate: 100 },
-      { name: "third", rate: 200 }
+    //remove new item
+    $scope.removeItem = function(item) {
+      var removedItem = $scope.items.indexOf(item);
+      $scope.items.splice(removedItem, 1);
+    };
+
+    //add an item
+    $scope.addItem = function() {
+      $scope.items.push({
+        name: $scope.item.name,
+        periority: {
+          title: $scope.item.periority.title,
+          color: `${
+            $scope.item.periority.title == "low"
+              ? "yellow"
+              : false || $scope.item.periority.title == "medium"
+              ? "green"
+              : false || $scope.item.periority.title == "high"
+              ? "red"
+              : false
+          }  `
+        },
+        avilable: true
+      });
+
+      //empty form after submitting add functionality
+      $scope.item.name = "";
+      $scope.item.periority.title = "";
+    };
+
+    //temporary data
+    $scope.items = [
+      {
+        name: "Eat",
+        avilable: true,
+        periority: {
+          title: "medium",
+          color: "green"
+        },
+        no: 2
+      },
+      {
+        name: "love",
+        avilable: true,
+        periority: {
+          title: "low",
+          color: "yellow"
+        },
+        no: 3
+      },
+      {
+        name: "pray",
+        avilable: true,
+        periority: {
+          title: "high",
+          color: "red"
+        },
+        no: 1
+      }
     ];
   }
 ]);
